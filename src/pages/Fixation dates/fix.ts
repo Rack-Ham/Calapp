c3a17690b2b56cc9464e8e76607ef9a1ed865616
jerome <jerome.expo@gmail.com>
2018-06-18 12:51:16 +0200

    Ajout des 2 pages précédemment oubliés

---
+++ src/pages/Fixation dates/fix.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+
+@Component({
+    selector: 'fix-home',
+    templateUrl: 'fix.html'
+  })
+  export class fixPage {
+  
+    constructor(public navCtrl: NavController) {
+  
+    }
+  }
+
+
+
+
+    
