780b24aa1831872a0accc77f67e586867e1f4032
jerome <jerome.expo@gmail.com>
2018-06-18 11:06:11 +0200

    Adding new page : fixation dates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,19 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-
 import { MyApp } from './app.component';
+
+//------------------Pages---------------------//
+//HOMEPAGE
 import { HomePage } from '../pages/home/home';
+//FIXATION DATES
+import { fixPage } from '../pages/Fixation dates/fix';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    fixPage
   ],
   imports: [
     BrowserModule,
@@ -19,7 +24,8 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    fixPage
   ],
   providers: [
     StatusBar,
